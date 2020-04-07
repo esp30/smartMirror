54a2125de8cec9def6dacfdbe4fb1356109459d9
JPCatarino <jorge.catarino@ua.pt>
2020-04-07 18:56:58 +0100

    [APP] Add smartMirror skeleton

---
+++ src/main/java/com/esp30/smartMirror/SmartMirrorApplication.java
@@ -0,0 +1,13 @@
+package com.esp30.smartMirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMirrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMirrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esp30/smartMirror/SmartMirrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esp30.smartMirror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMirrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
