f11952b412bf0e307b74b9ba948e574eabcd259d
Pedro Teixeira <pedro.teix@ua.pt>
2020-04-30 01:04:44 +0100

    [QA] Added structure for BDD tests
    -> Working but still don't know how to integrate with Jenkins

---
+++ src/test/java/com/esp30/smartMirror/cucumber/PatientStatusSteps.java
@@ -0,0 +1,34 @@
+package com.esp30.smartMirror.cucumber;
+
+/**
+ * smartMirror - com.esp30.smartMirror.cucumber.PatientStatusSteps <br>
+ *
+ * @author Pedro Teixeira pedro.teix@ua.pt
+ * @version 1.0 - April 30, 2020
+ */
+public class PatientStatusSteps {
+
+    @cucumber.api.java.en.And("^a medical professional that cares for said user$")
+    public void aMedicalProfessionalThatCaresForSaidUser() {
+    }
+
+    @cucumber.api.java.en.And("^a mobile app that accesses the system's public API$")
+    public void aMobileAppThatAccessesTheSystemSPublicAPI() {
+    }
+
+    @cucumber.api.java.en.When("^the system records a status \\(mood\\) change with the user$")
+    public void theSystemRecordsAStatusMoodChangeWithTheUser() {
+    }
+
+    @cucumber.api.java.en.Then("^the medical professional should be notified via the mobile app$")
+    public void theMedicalProfessionalShouldBeNotifiedViaTheMobileApp() {
+    }
+
+    @cucumber.api.java.en.When("^the medical professional wishes to check on their patient latest reports$")
+    public void theMedicalProfessionalWishesToCheckOnTheirPatientLatestReports() {
+    }
+
+    @cucumber.api.java.en.Then("^the medical professional should be able to access that information using the mobile app$")
+    public void theMedicalProfessionalShouldBeAbleToAccessThatInformationUsingTheMobileApp() {
+    }
+}
---
+++ src/test/java/com/esp30/smartMirror/cucumber/RemindersSteps.java
@@ -0,0 +1,26 @@
+package com.esp30.smartMirror.cucumber;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * smartMirror - com.esp30.smartMirror.cucumber.RemindersSteps <br>
+ *
+ * @author Pedro Teixeira pedro.teix@ua.pt
+ * @version 1.0 - April 30, 2020
+ */
+public class RemindersSteps {
+    @cucumber.api.java.en.Given("^a user$")
+    public void aUser() {
+        assertTrue(false); // TESTING PURPOSES -> SHOULD FAIL
+    }
+
+    @cucumber.api.java.en.When("^the user says \"([^\"]*)\"$")
+    public void theUserSays(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @cucumber.api.java.en.Then("^the smart mirror should set a reminder with an associated alarm set to go off at (\\d+) o'clock$")
+    public void theSmartMirrorShouldSetAReminderWithAnAssociatedAlarmSetToGoOffAtOClock(int arg0) {
+    }
+}
