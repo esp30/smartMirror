a075baf5b06a8a922a11b762689c787ce5e0c771
Fábio Alves <fabioalves98@ua.pt>
2020-05-29 12:27:50 +0100

    [DB] Moved to shared MySql Server

--- src/main/java/com/esp30/smartMirror/data/Emotion.java
+++ src/main/java/com/esp30/smartMirror/data/Emotion.java
@@ -1,12 +1,10 @@
 package com.esp30.smartMirror.data;
 
-import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.sql.Timestamp;
 
-@Data
 @Entity
 public class Emotion {
     private @Id @GeneratedValue Long id;
--- src/main/java/com/esp30/smartMirror/data/User.java
+++ src/main/java/com/esp30/smartMirror/data/User.java
@@ -1,11 +1,9 @@
 package com.esp30.smartMirror.data;
 
-import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Data
 @Entity
 public class User {
     private @Id @GeneratedValue Long id;
