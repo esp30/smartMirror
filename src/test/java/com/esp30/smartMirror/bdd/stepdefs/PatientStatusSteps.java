00e31a8e0ff09fe0d2f090ebe2c31918bc1e5c5a
Paulo Vasconcelos <paulobvasconcelos@gmail.com>
2020-05-31 05:21:42 +0100

    [QA] Feature 2, Scenario 1

--- src/test/java/com/esp30/smartMirror/bdd/stepdefs/PatientStatusSteps.java
+++ src/test/java/com/esp30/smartMirror/bdd/stepdefs/PatientStatusSteps.java
@@ -1,19 +1,42 @@
 package com.esp30.smartMirror.bdd.stepdefs;
+import com.esp30.smartMirror.data.Emotion;
 import cucumber.api.java.en.*;
+import com.esp30.smartMirror.controllers.ApiController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 
 public class PatientStatusSteps {
 
+    @Autowired ApiController controller;
+
+    boolean valid = true;
+    String patientName;
+
     // Scenario 1 - Emotion Trends
     @Given("a medical professional that needs to know the trends of emotions for all users")
-    public void aMedicalProfessionalThatNeedsToKnowTheTrendsOfEmotionsForAllUsers() {
+    public void aMedicalProfessionalThatNeedsToKnowTheTrendsOfEmotionsForAllUsers() throws Exception {
+        System.out.println("Doctor John wants to check on his patients.");
     }
 
     @And("a mobile app that accesses the system's public API")
     public void aMobileAppThatAccessesTheSystemSPublicAPI() {
+        try {
+            ArrayList<Emotion> emotions = controller.emotions();
+            System.out.println("===== EMOTIONS =====");
+            for (Emotion emo : emotions) {
+                System.out.println(emo.getUser() + ": " + emo.getValue());
+            }
+            System.out.println("====================");
+        } catch (Exception e) {
+            valid = false;
+            System.err.println("Error accessing API - emotions retrieval failed.");
+        }
     }
 
     @Then("the medical professional should be able to obtain all emotions registered without any user identification")
     public void theMedicalProfessionalShouldBeAbleToObtainAllEmotionsRegisteredWithoutAnyUserIdentification() {
+        assert(valid);
     }
 
     // Scenario 2 - Latest Reports
