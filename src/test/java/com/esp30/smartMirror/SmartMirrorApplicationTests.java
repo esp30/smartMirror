d86343e3263c8ce3ade35dc1373dd53be84faf6c
JPCatarino <jorge.catarino@ua.pt>
2020-06-01 02:37:46 +0100

    [TESTS] Add default emotion to test users

--- src/test/java/com/esp30/smartMirror/SmartMirrorApplicationTests.java
+++ src/test/java/com/esp30/smartMirror/SmartMirrorApplicationTests.java
@@ -1,13 +1,26 @@
 package com.esp30.smartMirror;
 
+import com.esp30.smartMirror.data.Emotion;
+import com.esp30.smartMirror.data.EmotionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SmartMirrorApplicationTests {
 
+	@Autowired
+	private EmotionRepository emotionRepository;
+
 	@Test
 	void contextLoads() {
+		Emotion emotionT = new Emotion("Tiago", "happy");
+		Emotion emotionM = new Emotion("Mariana", "sad");
+		Emotion emotionR = new Emotion("Raquel", "neutral");
+
+		emotionRepository.save(emotionT);
+		emotionRepository.save(emotionM);
+		emotionRepository.save(emotionR);
 	}
 
 }
